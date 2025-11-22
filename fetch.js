a670cca1c21ff57c067a63d3efc41aed117f1fa1
Dummage <mtnscu@gmail.com>
2025-11-22 14:40:09 +0200

    Fetch to be checked

--- fetch.js
+++ fetch.js
@@ -1,18 +1,32 @@
 const pool = require('./db');
 
-async function fetchVideo(videoURL) {
+async function fetchBlocker(videoURL) {
     try {
         const params = await processVideoURL(videoURL);
         const queryForVideoID = "SELECT id FROM Video WHERE platform = $1 AND video_id = $2";
 
-        client = await pool.connect();
+        let client =  await pool.connect();
 
         const result = await client.query(queryForVideoID, [params.platform, params.video_id]);
 
+        if(result.rows.length > 0) {
+            const video_id = result.rows[0].id;
+            const queryForBlockers =
+                "SELECT id, start_time_ms, end_time_ms, description FROM Blocker WHERE video_id = $1";
+            const blockersResult = await client.query(queryForBlockers, [video_id]);
+
+            if(blockersResult.rows.length > 0) {
+                return blockers =
+                    blockersResult.rows.map(row => new Blocker(row.id, row.start_time_ms, row.end_time_ms, row.description));
+
+            } else throw new Error("No blockers found for this video");
+        } else throw new Error("Video not found");
 
     } catch (error) {
-        console.error('Error processing video URL:', error);
+        console.error(error);
         throw error;
+    } finally {
+        if (client) client.release();
     }
 }
 
@@ -23,4 +37,13 @@ async function processVideoURL(videoURL) {
         return { platform, video_id };
     }
     else throw new Error('Unsupported platform');
+}
+
+class Blocker {
+    constructor(id, start_time_ms, end_time_ms, description) {
+        this.id = id;
+        this.start_time_ms = start_time_ms;
+        this.end_time_ms = end_time_ms;
+        this.description = description;
+    }
 }
